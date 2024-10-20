da01ead0ca4aae2c0986e47886615425f93f55a9
Tchinasso <thiago.chinasso@quiteja.com.br>
2024-10-19 22:42:16 -0300

    fix

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-import app from './app';
-import { PORT } from './config';
+import app from '../src/app';
+import { PORT } from '../src/config';
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
